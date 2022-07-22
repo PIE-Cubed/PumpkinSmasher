ce4bfb1e1d7ae071dbf2d574befc57a9dac34164
Cameron Gray <cam9cam9cam9@gmail.com>
2022-07-21 21:05:54 -0400

    Initial code for tank drive.

--- PumpkinSmasher/src/main/java/frc/robot/Drive.java
+++ PumpkinSmasher/src/main/java/frc/robot/Drive.java
@@ -1,5 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+
 public class Drive {
-    
+    private PWMTalonSRX leftMotorController;
+    private PWMTalonSRX rightMotorController;
+    private DifferentialDrive diffDrive;
+
+    public Drive()  {
+        leftMotorController  = new PWMTalonSRX(2);
+        rightMotorController = new PWMTalonSRX(1);
+        diffDrive = new DifferentialDrive(leftMotorController, rightMotorController);
+    }
+
+
+    public void drive(double leftPower, double rightPower)  {
+        diffDrive.tankDrive(leftPower, rightPower);
+    }
+
+
 }
