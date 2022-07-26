0359c3539e626fcaf8ead701043b01397dcfe1df
Cameron Gray <cam9cam9cam9@gmail.com>
2022-07-26 19:50:06 -0400

    Comments were added to make code easier to understand.

--- PumpkinSmasher/src/main/java/frc/robot/Drive.java
+++ PumpkinSmasher/src/main/java/frc/robot/Drive.java
@@ -1,23 +1,25 @@
 package frc.robot;
 
+// Imports
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 
+
 public class Drive {
+    // Declaring objects
     private PWMTalonSRX leftMotorController;
     private PWMTalonSRX rightMotorController;
     private DifferentialDrive diffDrive;
 
+    // Constructor to create drive object
     public Drive()  {
         leftMotorController  = new PWMTalonSRX(2);
         rightMotorController = new PWMTalonSRX(1);
         diffDrive = new DifferentialDrive(leftMotorController, rightMotorController);
     }
 
-
+    // Function for giving motors power
     public void drive(double leftPower, double rightPower)  {
         diffDrive.tankDrive(leftPower, rightPower);
     }
-
-
 }
