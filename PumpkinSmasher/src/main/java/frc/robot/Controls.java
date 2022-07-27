0ed5a4d0fa267f3c76a6df9082ae017dc7f8256d
Austin Conaway <a8conaway@gmail.com>
2022-07-26 20:49:39 -0400

    v1 of controller class, untested

--- PumpkinSmasher/src/main/java/frc/robot/Controls.java
+++ PumpkinSmasher/src/main/java/frc/robot/Controls.java
@@ -5,8 +5,53 @@ import edu.wpi.first.wpilibj.XboxController;
 public class Controls {
     //
     private final int portID = 1;
+    private XboxController xbox;
 
-    /**
-     * 
-     */
+    public Controls()   {
+        xbox = new XboxController (portID);
+    }
+
+    //Gets left motor power
+    public double getLeftMotorPower() {
+        return xbox.getLeftY();
+    }
+
+    //Gets right motor power
+    public double getRightMotorPower() {
+        return xbox.getRightY();
+    }
+
+    public boolean deployPlateCylinder() {
+        boolean leftBump;
+        boolean rightBump;
+
+        leftBump  = xbox.getLeftBumperPressed();
+        rightBump = xbox.getRightBumperPressed();
+        if ((leftBump == true) && (rightBump == true)) {
+            return false;
+        }
+        else if ((leftBump == true) && (rightBump == false)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean retractPlateCylinder() {
+        boolean leftBump;
+        boolean rightBump;
+
+        leftBump  = xbox.getLeftBumperPressed();
+        rightBump = xbox.getRightBumperPressed();
+        if ((leftBump == true) && (rightBump == true)) {
+            return false;
+        }
+        else if ((leftBump == false) && (rightBump == true)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
